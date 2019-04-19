ef49e6751aaa144b0353056366b238382a420dc3
Tanmay Verma <tanmay2592@gmail.com>
2019-04-19 18:46:45 -0500

    Initial commit with an example code and broadcast

---
+++ graph_config.py
@@ -0,0 +1,11 @@
+network1 = dict(
+    # Records the total number of nodes in the system
+    total_nodes = 4,
+
+    # The graph edges in the network 
+    node1 = [2],
+    node2 = [3,4],
+    node3 = [],
+    node4 = [1]
+)
+
