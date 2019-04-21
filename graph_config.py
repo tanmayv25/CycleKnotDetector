34507ee9fddd37e9399d0c38334b1407477421dc
habib-baig <mhhabib7@gmail.com>
2019-04-21 06:03:33 -0500

    Added MORE test cases

--- graph_config.py
+++ graph_config.py
@@ -37,4 +37,38 @@ network3 = dict(
     node5 = [4],
     node6 = [5]
 )
-test = network1
+
+network4 = dict(
+    # Records the total number of nodes in the system
+    total_nodes = 10,
+    initiator =1,
+    # The graph edges in the network
+    node1 = [2],
+    node2 = [3],
+    node3 = [4],
+    node4 = [5],
+    node5 = [6,7],
+    node6 = [8],
+    node7=  [10],
+    node8 = [9],
+    node9 = [1],
+    node10 = []
+)
+
+network5 = dict(
+    # Records the total number of nodes in the system
+    total_nodes = 10,
+    initiator =1,
+    # The graph edges in the network
+    node1 = [2],
+    node2 = [3],
+    node3 = [4],
+    node4 = [5],
+    node5 = [6,7],
+    node6 = [8],
+    node7=  [10],
+    node8 = [9],
+    node9 = [1],
+    node10 = [9]
+)
+test = network5
