1d9d02ebe76230c416949ec1a506eed29d4ae021
habib-baig <mhhabib7@gmail.com>
2019-04-21 05:56:30 -0500

    Working code

--- graph_config.py
+++ graph_config.py
@@ -1,11 +1,40 @@
+
 network1 = dict(
     # Records the total number of nodes in the system
     total_nodes = 4,
-
-    # The graph edges in the network 
+    initiator =1,
+    # The graph edges in the network
     node1 = [2],
-    node2 = [3,4],
+    node2 = [4,3],
     node3 = [],
     node4 = [1]
 )
 
+network2 = dict(
+    # Records the total number of nodes in the system
+    total_nodes = 8,
+    initiator =3,
+    # The graph edges in the network
+    node1 = [2],
+    node2 = [4,3],
+    node3 = [5,7],
+    node4 = [1],
+    node5 = [6],
+    node6 = [8],
+    node7=  [6],
+    node8 = [3]
+)
+
+network3 = dict(
+    # Records the total number of nodes in the system
+    total_nodes = 6,
+    initiator =4,
+    # The graph edges in the network
+    node1 = [4],
+    node2 = [3],
+    node3 = [2,4],
+    node4 = [2,6],
+    node5 = [4],
+    node6 = [5]
+)
+test = network1
